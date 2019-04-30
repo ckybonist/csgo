68d2fd5622372def565adca94be34b45e97ebd01
KuanYu Chu <ckybonist@gmail.com>
2019-04-30 18:57:06 +0800

    Optimize Heap's algorithm by removing unnecessary swaps

--- problem/perm.go
+++ problem/perm.go
@@ -18,16 +18,20 @@ func heapPerm(s []string, size int, cap int, count *int) {
 
 	for i := 0; i < size; i++ {
 		heapPerm(s, size - 1, cap, count)
-		if size % 2 == 1 {
-			s[0], s[size - 1] = swap(s[0], s[size - 1])
-		} else {
-			s[i], s[size - 1] = swap(s[i], s[size - 1])
+
+		
+		if i < size - 1 {  // avoid unnecessary swaps when i == k-1
+			if size % 2 == 1 {
+				s[0], s[size - 1] = swap(s[0], s[size - 1])
+			} else {
+				s[i], s[size - 1] = swap(s[i], s[size - 1])
+			}
 		}
 	}
 }
 
 func Perm() {
-	s := strings.Split("abcd", "")
+	s := strings.Split("abc", "")
 	fmt.Printf("Permutations of %v:\n", s)
 	count := 0
 	heapPerm(s, len(s), len(s), &count)
